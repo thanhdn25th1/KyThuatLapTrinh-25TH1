a570c24ee08d31d29f60312615d48723921e9920
Buoi2 <PMT201@PMT3047>
2026-03-27 10:31:49 +0700

    buoi1

--- Kythuatlaptrinh/buoi1/Buoi1_1.cpp
+++ Kythuatlaptrinh/buoi1/Buoi1_1.cpp
@@ -49,9 +49,66 @@ void RemovePerson(vector<Person>& p, int id) {
 	}
 	cout << "Not found person with id: " << id << endl;
 }
+bool FindPerson(vector<Person>p, string name) {
+	for (Person i : p) {
+		if (i.name == name) {
+			return true;
+		}
+	}
+	return false;
+}
 
+void ExportToFile(vector<Person>p, string flieName) {
+	ofstream file(filename, ios::binary);
+	if (!file) {
+		cout << "Can't open this file" << endl;
+		return;
+	}
+	for (Person i : p) {
+		file.write(reinterpret_cast<const char*>(&i.id), sizeof(i.id));
 
+		size_t namelength = i.name.size();
+		file.write(reinterpret_cast<const char*>(&namelength), sizeof(namelength));
+		file.write(i.name.c_str(), namelength);
+		file.write(reinterpret_cast<const char*>(&i.age), sizeof(i.age));
 
+		size_t addresslength = i.address.size();
+		file.write(reinterpret_cast<const char*>(&addresslength), sizeof(addresslength));
+		file.write(i.address.c_str(), addresslength);
+	}
+}
+
+	void ReadFromFile(vector<Person>p, string flieName) {
+		ifstream file(filename, ios::binary);
+		if (!file) {
+			cout << "Can't open this file" << endl;
+			return;
+		}
+		/*while (p.size() > 0)
+		{
+			p.pop_back();
+		}*/
+		
+		Person i;
+		while(file.peek()!=EOF){
+
+			file.read(reinterpret_cast<const char*>(&i.id), sizeof(i.id));
+
+			size_t namelength;
+			file.read(reinterpret_cast<const char*>(&namelength), sizeof(namelength));
+			i.name.resize(namelength);
+			file.read(&i.name[0], namelength);
+
+			file.read(reinterpret_cast<const char*>(&i.age), sizeof(i.age));
+
+			size_t addresslength = i.address.size();
+			file.read(reinterpret_cast<const char*>(&addresslength), sizeof(addresslength));
+			i.address.resize(addresslength);
+			file.read(&i.address[0], addresslength);
+
+			RemovePerson(p, i.id);
+			p.push_back(i);
+		}
 
 
 
@@ -92,12 +149,26 @@ int main() {
 			break;
 		}
 		case 4: {
+			string name;
+			cout << "Input name to find: ";
+			cin.ignore();
+			getline(cin, name);
+			bool res = FindPerson(list, name);
+			if (res) {
+				cout << "Found person with name" << name << endl;
+			}
+			else
+				cout << "Person isn't existed" << endl;
 			break;
 		}
 		case 5: {
+			ExportToFile(list, "25TH1.dla");
+			cout << " Export successfully" << endl;
 			break;
 		}
 		case 6: {
+			ReadFromFile(list, "25TH1.dla");
+			cout << "Import successfully" << endl;
 			break;
 		}
 		case 0: {
